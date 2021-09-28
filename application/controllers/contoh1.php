ecd9fcac62cc807784e16d4e2858604577c93be0
dhitaaugst <dhitaagstp@gmail.com>
2021-09-28 22:19:01 +0700

    menambahkan application/,system/,user_guide/,.editorconfig,.gitignore,composer.json,contributing.md,index.php,license.txt,readme.rst

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends c1_controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        ech "Nama Saya Dhita Augusta Pratiwi
+        Saya Tinggal di daerah Tangerang
+        Olahraga yang saya sukai adalah Berenang";
+        
+    }
+}
