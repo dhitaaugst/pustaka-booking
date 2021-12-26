6adb1fbaa26fa123a68b8cf54df612654eddeb8b
dhitaaugst <dhitaagstp@gmail.com>
2021-12-26 21:35:06 +0700

    menambahkan application/controllers/Web.php,application/views/n,application/views/v_about.php,application/views/v_footer.php,application/views/v_header.php,application/views/v_index.php,assets/css/stylebuku.css

---
+++ application/views/v_about.php
@@ -0,0 +1,19 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan: Dhita</li>
+        <li>Nama Belakang: Augusta</>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Bumi Indah Tahap 3 Jl.Cempaka Ix Blok HC.7 Pasarkemis</li>
+    </ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Berenang</li>
+        <li>Bulutangkis</li>
+    </ul>
+</section>
